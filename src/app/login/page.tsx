3b9fa388f7360886dbacc9abda3518c7dafd2728
Albert Smaczylo <106358214+albberrrt@users.noreply.github.com>
2023-08-15 13:46:45 -0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,11 +5,7 @@ import Button from '@/components/Button/Button'
 export default function login() {
   return (
     <main>
-      <div>
-        <Input name='email' placeholder='Digite seu email'/>
-        <Input name='password' type='password' placeholder='Digite sua senha'/>
-        <Button name='btn' id='btn' value='Entrar'/>
-      </div>
+      <h1>Login</h1>
     </main>
   )
 }
