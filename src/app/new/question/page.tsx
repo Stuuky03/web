6aedca08fe41050d2327b90878bda88bcefd974e
albberrrt.yayay <albberrrt.yayay@gmail.com>
2023-10-23 11:37:48 -0300

    feat: created New Question page

--- src/app/new/question/page.tsx
+++ src/app/new/question/page.tsx
@@ -1,15 +1,25 @@
+"use client"
+
 import Markdown from "react-markdown"
 import "./style.scss"
 import PageHeader from "@/components/organisms/PageHeader/PageHeader"
+import { useState } from "react"
 
 const NewQuestion = () => {
+  const [postContent, setPostContent] = useState("")
+
   return (
     <>
       <PageHeader />
       <main>
         <div className="main-content">
           <section>
-            <Markdown>{`# OLÁÁÁÁÁÁ`}</Markdown>
+            <textarea value={postContent} onChange={e => setPostContent(e.target.value)} />
+          </section>
+          <section>
+            <div className="markdown-body">
+              <Markdown>{postContent}</Markdown>
+            </div>
           </section>
         </div>
       </main>
